7f995228c95c674054ebff41afd27e99b034964e
Llorleon24 <carlosmartinllorente0@gmail.com>
2024-11-08 11:45:18 +0100

    reposistorio eamen romero

--- carlos/routes/index.js
+++ carlos/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var datos = require("../data/dataprovider");
+ 
+ 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Inicio' });
 });
-
+ 
+ 
+router.get('/index', function(req, res, next) {
+    const posts = datos.getAllPosts();
+    res.render("index", { posts: posts, post: null, title: "Inicio de blog" });
+});
+ 
+ 
+router.get('/index/:id', function(req, res, next) {
+    const posts = datos.getAllPosts();
+    const post = datos.getPostById(req.params.id);
+    res.render("index", { posts: posts, post: post, title: post ? post.title : "Artículo no encontrado" });
+});
+ 
 module.exports = router;
